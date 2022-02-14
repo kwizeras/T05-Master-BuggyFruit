2902bb74f53588f7b88ace5906c4b28f8aa15ac2
shepherdp <88725439+shepherdp@users.noreply.github.com>
2022-02-14 18:09:42 -0500

    Update t5_buggy_circle_area.py

--- t5_buggy_circle_area.py
+++ t5_buggy_circle_area.py
@@ -1,6 +1,6 @@
 ######################################################################
-# Author: Dr. Jan Pearce and Patrick Shepherd      TODO: Change this to your names
-# Username: pearcej, shepherdp                     TODO: Change this to your usernames
+# Author: Dr. Patrick Shepherd      TODO: Change this to your names
+# Username: shepherdp               TODO: Change this to your usernames
 #
 # Assignment: T5: Buggy Fruit
 # Purpose: A flawed program which uses a function to determine the area of a circle
@@ -18,22 +18,20 @@
 # Attribution-Noncommercial-Share Alike 3.0 United States License.
 ####################################################################################
 
-import sys                          #needed for unit testing
+
+from inspect import getframeinfo, stack
+
 r=0                                 #FIXME: This variable should not be here.  Please fix.
 
-def testit(did_pass): #This function is correct
+def unittest(did_pass):
     """
     Print the result of a unit test.
-
-    :param did_pass: Boolean value representing a test case
+    :param did_pass: a boolean representing the test
     :return: None
     """
 
-    # This function may cause an error due to an update in Python.
-    # If so, comment the following line out, as well as the lines under the
-    # 'if' and 'else' statements, and replace them with a simple print
-    # such as 'print( 'Passed' )' or 'print( 'Failed' )'.
-    linenum = sys._getframe(1).f_lineno # Gets the caller's line number.
+    caller = getframeinfo(stack()[1][0])
+    linenum = caller.lineno
     if did_pass:
         msg = "Test at line {0} ok.".format(linenum)
     else:
@@ -53,9 +51,9 @@ def circleArea_test_suite(): # These until tests are correct
     :return: None
     """
     print("\nRunning circleArea_test_suite()).")
-    testit(circleArea(0)==0)
-    testit(circleArea(1)==3.141592)
-    testit(circleArea(2.5)==3.141592*2.5*2.5)
+    unittest(circleArea(0)==0)
+    unittest(circleArea(1)==3.141592)
+    unittest(circleArea(2.5)==3.141592*2.5*2.5)
 
 def main():
     # This is where the main program starts
