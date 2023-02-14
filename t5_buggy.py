3bac1affe4f81074b767aab38600edde8677dd19
shepherdp <88725439+shepherdp@users.noreply.github.com>
2023-02-14 15:32:28 -0500

    Update t5_buggy.py

--- t5_buggy.py
+++ t5_buggy.py
@@ -18,24 +18,48 @@
 
 import random
 
-def display_result(n1, n2, sum):
+
+def squareroot(x):
     """
-    Displays the results of the calculation, and whether it is correct or not.
-
-    :param n1: a random integer
-    :param n2: another random integer
-    :param sum: a third random integer
-    :return: None
+    Calculates the square root of a number
+    :param x: a number to take the square root of
+    :return: the square root of x
     """
+    answer = x ** 1 / 2
+    return answer
 
-    print ("The first number was ", n1)
-    print ("The second number was ", n2)
-    print ("The computer guessed ", int(n1), " + ", int(n2), " = ", int(sum))
-    if sum == n1 + n2:
-        print('Correct!')
-    else:
-        print('Wrong!')
+def squared(x):
+    """
+    Calculates the square of a number
+    :param x: a number to square
+    :return: the square of x
+    """
+    answer = x ** 2
 
+    # I round the answer to 5 decimal places to avoid any representation errors so
+    # that I can reliably compare answers between different methods of calculating
+    # the same thing.
+    answer = round(answer, 5)
+    return answer
+def hypoteneuse(a, b):
+    """
+    Calculates the hypotenuse length on a triangle with other side lengths a and b
+    :param a: the length of one side of the triangle
+    :param b: the length of another side of the triangle
+    :return: the length of the hypotenuse connecting the two sides a and b
+    """
+    val = 0
+    val = val + squared(a)
+    val = val + squared(b)
+    val = squareroot(val)
+
+    return val
+
+    # Note that this function could also have been written in a single line:
+    # return squareroot(squared(a) + squared(b))
+
+    # Breaking it down into individual pieces as we did above can help make
+    # one-line solutions clearer to put together!
 
 def main():
     """
@@ -43,10 +67,25 @@ def main():
 
     :return: None
     """
-    number1 = random.randint(1, 2)
-    number2 = random.randint(1, 2)
 
-    answer = random.random() * 4
-    display_result(number1, number2, answer)
+    print('Welcome to exploring the debugger!!!')
+    print('There is a bug lurking somewhere deep in the darkest reaches of the code above.')
+
+    # Notice how I put a little space between lines with the \n below?
+    print('It is your job to hunt it down! (NOT fix it, just find it!)\n')
+
+    number1 = random.randint(1, 5)
+    number2 = random.randint(1, 8)
+
+    length = hypoteneuse(number1, number2)
+
+    # The lines below uses something called an 'f-string' for string
+    # formatting.  Feel free to use this as a model for making your
+    # own formatted strings!
+    print(f'The values I chose were {number1} and {number2}')
+    print(f'The value I calculated was: {length}')
+
+    if not (squared(length) == squared(number1) + squared(number2)):
+        print('Something went wrong!  These numbers don\'t add up the way they should!')
 
 main()
